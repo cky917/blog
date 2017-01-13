79055e32468e6ac011d2d352200e0cfa918fb6a5
cky <576779975@qq.com>
2017-01-13 11:39:46 +0800

    目录样式优化

--- themes/mabao-theme/source/js/index.js
+++ themes/mabao-theme/source/js/index.js
@@ -14,22 +14,34 @@
     });
 
     function eventHandler(){
+       
+        countCateStyle();
         $(window).scroll(function(){
-            var titleList = $('.post-content').find('h1,h2,h3,h4,h5');
-            for(var i =0,len=titleList.length;i<len;i++){
-                var $this = $(titleList[i]);
-                var distance = $this.offset().top-$(document).scrollTop();
-                var id;
-                var cateObj;
-
-                if(distance < 60){
-                    id = $this.attr('id');
-                    cateObj = $('#toc').find('[href="#'+ id +'"]');
-                    $('.toc-item').removeClass('toc-active');
-                    cateObj.parents('.toc-item').addClass('toc-active');
-                }
-            }
+            countCateStyle();
         });
     }
-
+    function countCateStyle(){
+        var cateList = $('.toc-item');
+        var titleList = $('.post-content').find('h1,h2,h3,h4,h5');
+        var tocObj = $('.toc-article');
+        if($(document).scrollTop() > 550){
+            tocObj.addClass('toc-article-fixed');
+        }else{
+            tocObj.removeClass('toc-article-fixed');
+        }
+        for(var i =0,len=titleList.length;i<len;i++){
+            var $this = $(titleList[i]);
+            var distance = $this.offset().top-$(document).scrollTop();
+            var id;
+            var cateObj,cateLiObj;
+            if(distance < 60){
+                id = $this.attr('id');
+                cateObj = $('#toc').find('[href="#'+ id +'"]');
+                cateLiObj = cateObj.parents('.toc-item');
+                cateList.removeClass('toc-active');
+                cateLiObj.addClass('toc-active');
+                continue;
+            }
+        }
+    }
 }(jQuery));
